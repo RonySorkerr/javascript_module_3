9f8fb9fd253b76b9faab7e761b8917a0515c43b9
RonySorkerr <mdronysarkar33299@gmail.com>
2024-01-27 22:25:23 +0600

    array membership checked

---
+++ pra_3.js
@@ -0,0 +1,9 @@
+// array of different book names
+const bookNames =  ['harry potter', 'homayon azad', 'arif azad', 'bela sheshe'];
+// making a condition if the book names is included
+if(bookNames.includes('javascript')){
+    console.log('Your book is included here');
+}
+else{
+    console.log('Your book is not included');
+}
