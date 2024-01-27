2238f963d57b784c0849812bca61d9abd5864765
RonySorkerr <mdronysarkar33299@gmail.com>
2024-01-27 22:45:13 +0600

    practicing

---
+++ combine.js
@@ -0,0 +1,6 @@
+let name = ['jamal', 'michka', 'fakir mia'];
+let sName = ['farzana', 'jannatul', 'sumaiya'];
+// concatanating
+console.log(name);
+console.log(sName);
+let result = name.concat(sName);
