d2541386f21ab77dde97900f05b8f3e6445365ae
RonySorkerr <mdronysarkar33299@gmail.com>
2024-01-27 22:34:03 +0600

    checking array done

---
+++ pra_4.js
@@ -0,0 +1,26 @@
+// making array
+const names = ['jamal', 63, 69, 'kamal'];
+const age = [];
+const rich = false;
+// conditions
+if(Array.isArray(names)){
+    console.log('namse variable is an array')
+}
+else(
+    console.log('names varialbe is not an array')
+
+)
+// second var condition
+if(Array.isArray(age)){
+    console.log('age variable is an array');
+}
+else{
+    console.log('age var is not an array');
+}
+// third var condition
+if(Array.isArray(rich)){
+    console.log('you are rich');
+}
+else{
+    console.log('you are a fakir');
+}
