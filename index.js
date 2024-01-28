09680aee08db62a4fe25a9bd513adab882746377
RonySorkerr <mdronysarkar33299@gmail.com>
2024-01-28 18:22:20 +0600

    switch conditional learning

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 const names =  ['kamal', 'jamal', 'ratan', 'fatema'];
 const culprit =  names[2];
 console.log(culprit);
-d
---
+++ switch.js
@@ -0,0 +1,20 @@
+// light condition
+const light = true;
+switch (light) {
+    case light:
+        console.log('Your light is on.');
+        break;
+
+    default:
+        console.log('Your light is off.')
+        break;
+}
+// grading condition
+
+function gradingCalc(score){
+    let gradingCalc
+        switch(true){
+            case (score >= 100 && score <=90):
+                gradingScore = "A+"
+        }
+}
